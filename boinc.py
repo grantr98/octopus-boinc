d4f2a743fa66e838e74634e41875fa263e3a9408
Grant Rodgers <github@grejurn.org>
2026-05-15 15:39:40 +0100

    chore(modern): Modernisation and cleanup with better knowledge than I had before.

--- boinc.py
+++ boinc.py
@@ -3,25 +3,30 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import json
-from datetime import datetime
+import datetime
 from sys import platform
 import logging
+
 retries = 0
 
+
 def fetch():  # GET current tariff data from Octopus API
-    url = "{url}/v1/products/{prod_code}/electricity-tariffs/{tariff}/standard-unit-rates".format(url=os.getenv('BASE_URL'),
-                                                                                      prod_code=os.getenv(
-                                                                                          'PRODUCT_CODE'),
-                                                                                      tariff=os.getenv('TARIFF'))
-    logging.debug("GET URL " + url)
-    key = os.getenv('KEY')
-    response = requests.get(url, headers={
-        "X-RapidAPI-Key": key
-    })
+    url = "{url}/v1/products/{prod_code}/electricity-tariffs/{tariff}/standard-unit-rates".format(
+        url=os.getenv("BASE_URL"),
+        prod_code=os.getenv("PRODUCT_CODE"),
+        tariff=os.getenv("TARIFF"),
+    )
+    logging.info("GET URL " + url)
+    key = os.getenv("KEY")
+    response = requests.get(url, headers={"X-RapidAPI-Key": key})
     if response.status_code == 200:  # if success, continue. If not, die.
         return json.loads(response.text)
     else:
-        logging.error("Unable to get rates. Response code {code}. Exiting".format(code=str(response.status_code)))
+        logging.error(
+            "Unable to get rates. Response code {code}. Exiting".format(
+                code=str(response.status_code)
+            )
+        )
         exit(0)
 
 
@@ -32,7 +37,9 @@ def round_down(t):  # round time down to nearest half-hour
         return t.replace(second=0, microsecond=0, minute=0)
 
 
-def round_up(t):  # round time up to nearest half-hour (almost to bodge around midnight +1 crashing)
+def round_up(
+    t,
+):  # round time up to nearest half-hour (almost to bodge around midnight +1 crashing)
     if t.minute >= 30:
         return t.replace(second=59, microsecond=0, minute=59)
     else:
@@ -42,9 +49,13 @@ def round_up(t):  # round time up to nearest half-hour (almost to bodge around m
 def cur_price(data, time):
     result = 101.00  # default value always higher than possible on Agile so will not start BOINC if it cannot find data
     for a in data:
-        if a['valid_from'] == time:
-            logging.debug(
-                "Located: {valid_from} at price {price}".format(valid_from=a['valid_from'], price=a['value_inc_vat']))
+        timestamp = datetime.datetime.fromisoformat(a["valid_from"])
+        if timestamp == time:
+            logging.info(
+                "Located: {valid_from} at price {price}".format(
+                    valid_from=timestamp, price=a["value_inc_vat"]
+                )
+            )
             result = a
             break
 
@@ -52,46 +63,61 @@ def cur_price(data, time):
 
 
 def boinc(price, runtime):
-    threshold = float(os.getenv('PRICE_THRESHOLD'))
+    threshold = float(os.getenv("PRICE_THRESHOLD"))
     boinc_path = "."
 
     if platform == "linux" or platform == "linux2":
-        boinc_path = os.getenv('LINUX_BOINC')
+        boinc_path = os.getenv("LINUX_BOINC")
     elif platform == "darwin":
-        boinc_path = os.getenv('MAC_BOINC')
+        boinc_path = os.getenv("MAC_BOINC")
     elif platform == "win32":
-        boinc_path = os.getenv('WIN_BOINC')
+        boinc_path = os.getenv("WIN_BOINC")
     else:
         logging.error("Unable to determine platform")
 
     if price <= threshold:
-        logging.info("Price {price} less than threshold {th}".format(price=str(price), th=str(threshold)))
-        os.system(boinc_path + " --set_run_mode auto {run_t}".format(run_t=str(runtime)))
+        logging.info(
+            "Price {price} less than threshold {th}".format(
+                price=str(price), th=str(threshold)
+            )
+        )
+        os.system(
+            boinc_path + " --set_run_mode auto {run_t}".format(run_t=str(runtime))
+        )
     else:
-        logging.info("Price {price} is greater than threshold {th}".format(price=str(price), th=str(threshold)))
+        logging.info(
+            "Price {price} is greater than threshold {th}".format(
+                price=str(price), th=str(threshold)
+            )
+        )
         os.system(boinc_path + " --set_run_mode never")
 
 
 def main():
-    path = join(dirname(__file__), '.env')
-    logging.basicConfig(filename='boinc{date}.log'.format(date=datetime.now().date()), format='%(asctime)s %(message)s',
-                        level=logging.DEBUG)
+    path = join(dirname(__file__), ".env")
+    logging.basicConfig(
+        filename="boinc{date}.log".format(
+            date=datetime.datetime.now(tz=datetime.UTC).date()
+        ),
+        format="%(asctime)s %(message)s",
+        level=logging.DEBUG,
+    )
     load_dotenv(path)
-    now = datetime.utcnow()
+    now = datetime.datetime.now(tz=datetime.UTC)
 
     rd = round_down(t=now)
     ru = round_up(t=now)
 
-    logging.debug("Rounded time " + str(rd))
+    logging.info("Rounded time " + str(rd))
 
-    a = fetch()['results']
+    a = fetch()["results"]
 
-    price_now = cur_price(data=a, time=rd.isoformat() + "Z")  # Octopus adds "Z" to end of ISO Time, python doesn't
-    logging.debug("Current price {cp}".format(cp=str(price_now['value_inc_vat'])))
+    price_now = cur_price(data=a, time=rd)
+    logging.info("Current price {cp}".format(cp=str(price_now)))
     time_diff = (ru - now).seconds
-    logging.debug("Time client will be run for: {time} ".format(time=str(time_diff)))
-    boinc(price=price_now['value_inc_vat'], runtime=time_diff)
+    logging.info("Time client will be run for: {time} ".format(time=str(time_diff)))
+    boinc(price=price_now["value_inc_vat"], runtime=time_diff)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- boinc_prometheus.py
+++ boinc_prometheus.py
@@ -23,9 +23,12 @@ def round_down(t):  # round time down to nearest half-hour
 def cur_price(data, time):
     result = 101.00
     for a in data:
-        if a['valid_from'] == time:
+        if a["valid_from"] == time:
             logging.debug(
-                "Located: {valid_from} {data}".format(valid_from=a['valid_from'], data=a))
+                "Located: {valid_from} {data}".format(
+                    valid_from=a["valid_from"], data=a
+                )
+            )
             result = a
             break
 
@@ -35,72 +38,100 @@ def cur_price(data, time):
 @cached(cache=TTLCache(maxsize=2048, ttl=720))
 def fetch_unit_rates():  # GET current tariff data from Octopus API
     url = "{url}/v1/products/{prod_code}/electricity-tariffs/{tariff}/standard-unit-rates".format(
-        url=os.getenv('BASE_URL'),
-        prod_code=os.getenv(
-            'PRODUCT_CODE'),
-        tariff=os.getenv('TARIFF'))
+        url=os.getenv("BASE_URL"),
+        prod_code=os.getenv("PRODUCT_CODE"),
+        tariff=os.getenv("TARIFF"),
+    )
     logging.debug("GET URL " + url)
 
     response = requests.get(url)
     if response.status_code == 200:  # if success, continue. If not, die.
         return json.loads(response.text)
     else:
-        logging.error("Unable to get rates. Response code {code}. Exiting".format(code=str(response.status_code)))
+        logging.error(
+            "Unable to get rates. Response code {code}. Exiting".format(
+                code=str(response.status_code)
+            )
+        )
         exit(0)
 
 
 @cached(cache=TTLCache(maxsize=2048, ttl=180))
 def fetch_consumption():  # GET consumption data from Octopus API
-    url = "{url}/v1/electricity-meter-points/{mpan}/meters/{serial_number}/consumption/" \
-          "?group_by=day&page_size=2&order_by=-period".format(
-        url=os.getenv('BASE_URL'), mpan=os.getenv('MPAN'), serial_number=os.getenv('ELEC_SERIAL_NUMBER'))
+    url = (
+        "{url}/v1/electricity-meter-points/{mpan}/meters/{serial_number}/consumption/"
+        "?group_by=day&page_size=2&order_by=-period".format(
+            url=os.getenv("BASE_URL"),
+            mpan=os.getenv("MPAN"),
+            serial_number=os.getenv("ELEC_SERIAL_NUMBER"),
+        )
+    )
     logging.debug("GET URL " + url)
-    key = os.getenv('KEY')
-    response = requests.get(url=url, auth=(key, ''))
+    key = os.getenv("KEY")
+    response = requests.get(url=url, auth=(key, ""))
     if response.status_code == 200:  # if success, continue. If not, die.
         return json.loads(response.text)
     else:
-        logging.error("Unable to get rates. Response code {code}. Exiting".format(code=str(response.status_code)))
+        logging.error(
+            "Unable to get rates. Response code {code}. Exiting".format(
+                code=str(response.status_code)
+            )
+        )
         exit(0)
 
 
 # Create a metric to track time spent and requests made.
-CURRENT_PRICE_INC_VAT = Gauge('current_electricity_price_inc_vat', 'Current Electricity price including VAT')
-CURRENT_PRICE_EXC_VAT = Gauge('current_electricity_price_exc_vat', 'Current Electricity price excluding VAT')
-CONSUMPTION_YESTERDAY = Gauge('electricity_consumption_yesterday', 'Yesterdays Energy Use.')
+CURRENT_PRICE_INC_VAT = Gauge(
+    "current_electricity_price_inc_vat", "Current Electricity price including VAT"
+)
+CURRENT_PRICE_EXC_VAT = Gauge(
+    "current_electricity_price_exc_vat", "Current Electricity price excluding VAT"
+)
+CONSUMPTION_YESTERDAY = Gauge(
+    "electricity_consumption_yesterday", "Yesterdays Energy Use."
+)
 
 
 # Decorate function with metric.
 
 
 def set_current_price(time, unit_rates):
-    price = cur_price(data=unit_rates,
-                      time=time.isoformat() + "Z")  # Octopus adds "Z" to end of ISO Time, python doesn't
-    CURRENT_PRICE_INC_VAT.set(price['value_inc_vat'])
-    CURRENT_PRICE_EXC_VAT.set(price['value_exc_vat'])
+    price = cur_price(
+        data=unit_rates, time=time.isoformat() + "Z"
+    )  # Octopus adds "Z" to end of ISO Time, python doesn't
+    CURRENT_PRICE_INC_VAT.set(price["value_inc_vat"])
+    CURRENT_PRICE_EXC_VAT.set(price["value_exc_vat"])
 
 
 def set_consumption(consumption):
     logging.debug(
-        "Located: {valid_from} {data}".format(valid_from=consumption[0]['interval_start'], data=consumption[1]))
+        "Located: {valid_from} {data}".format(
+            valid_from=consumption[0]["interval_start"], data=consumption[1]
+        )
+    )
 
-    CONSUMPTION_YESTERDAY.set(consumption[1]['consumption'])
+    CONSUMPTION_YESTERDAY.set(consumption[1]["consumption"])
 
 
 def fetch_rates_and_update(rd_utc):
-    rates = fetch_unit_rates()['results']
-    consumption = fetch_consumption()['results']
+    rates = fetch_unit_rates()["results"]
+    consumption = fetch_consumption()["results"]
     set_current_price(rd_utc, rates)
     set_consumption(consumption)
     time.sleep(30)
 
 
-if __name__ == '__main__':
-    path = join(dirname(__file__), '.env')
+if __name__ == "__main__":
+    path = join(dirname(__file__), ".env")
 
-    logging.basicConfig(handlers=[logging.FileHandler('boinc{date}.log'.format(date=datetime.now().date())), logging.StreamHandler()], format='%(asctime)s %(message)s',
-                        level=logging.DEBUG)
-
+    logging.basicConfig(
+        handlers=[
+            logging.FileHandler("boinc{date}.log".format(date=datetime.now().date())),
+            logging.StreamHandler(),
+        ],
+        format="%(asctime)s %(message)s",
+        level=logging.DEBUG,
+    )
 
     load_dotenv(path)
 
@@ -113,7 +144,6 @@ if __name__ == '__main__':
     fetch_rates_and_update(rd_utc=rd_utc)
 
     while True:  # Now loop
-
         now_utc = datetime.utcnow()
 
         if now_utc.minute == 30 or now_utc.minute == 0:  # If on the hour or half hour:
